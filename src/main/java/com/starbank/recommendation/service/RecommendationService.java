2d1d0eb54c979b8c94839dcd0c0565878b5c91d4
Alex <alexbaturin@list.ru>
2026-06-21 11:22:16 +0800

    Сделано что требовалось.

--- src/main/java/com/starbank/recommendation/service/RecommendationService.java
+++ src/main/java/com/starbank/recommendation/service/RecommendationService.java
@@ -6,8 +6,6 @@ import com.starbank.recommendation.repository.RuleRepository;
 import com.starbank.recommendation.rule.RuleCheck;
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
